924fe3967d86dcfe9ba237d970fa9014525e97aa
JoshSkyner <joshskyner06@gmail.com>
2026-04-15 18:47:39 +0100

    Creates the Income Inputs. - Put it underneath the previously made secion - Can be moved to a different tab maybe later on.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import ExpenditureForm from './ExpenditureForm'
+import Income from './Income'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
       <ExpenditureForm />
+      <Income />
     </>
   )
 }
---
+++ src/Income.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react' ;
+
+const Income = () => {
+    //Define the globals
+    const [source, setSource] = useState("");
+    const [amount, setAmount] = useState("");
+    const [date, setDate] = useState("");
+    //Shows when save button is clicked
+    const handleSave = () => {
+            alert("Saved " + source + " (£" + amount + ") on " + date);
+        };
+
+    return(
+        <div style={{padding: '20px', border: '1px'}}>
+            <h3>Add Income Source</h3>
+
+            <input
+                type="text"
+                placeholder='Source Name'
+                value={source}
+                        //onChange is basically when the button is clicked
+                        //e.target.value is the actual value inside of the text box
+                        //setSource updates our saved value with the value inside of the text box
+                onChange={(e) => setSource(e.target.value)}
+            />
+            <input 
+                    type="number" 
+                    placeholder="Amount" 
+                    value={amount} 
+                    onChange={(e) => setAmount(e.target.value)} 
+            />
+
+                <input 
+                        //date data type creates the calender drop down
+                    type="date" 
+                    value={date} 
+                    onChange={(e) => setDate(e.target.value)} 
+            />
+            <button onClick={handleSave}>Save Entry</button>
+        </div>
+    );
+
+};
+
+export default Income;
